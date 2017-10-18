8919e8219f732f3c9d34019eaf1214bf34d2036a
Sausville <SausSam18@issaquah.wednet.edu>
2017-10-18 11:45:30 -0700

    pg 805 start

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         /*
+        Pg 802
         Problem 1: Recursion methods call themselves within their own method until a certain thing is met.
         It behaves similarly to a for loop as it repeats until a certain thing happens.
         Problem 2: Base cases are the cases that break the loop  and is the first thing it does after the loop of methods.
@@ -39,14 +40,67 @@ public class Main {
         Problem 8: It is the info or stack of method calls that need to happen for the loop to occur
         Problem 9: It would not reverse as you are printing the line before the method call
         Problem 10: An infinite loop or stack overflow happens as it calls the method forever
+        pg 805
+        Problem 11:
+
+        Problem 12:
+
+        Problem 13:
+
+        Problem 14:
+
+        Problem 15:
+
+        Problem 16:
+
+        Problem 17:
+
+        Problem 18:
+
+        Problem 19:
+
          */
-
-
         doubleReverse("YEET");
         System.out.println();
+        //Pg 808
+        //Problem 2
+        writeNums(5);
+        System.out.println();
+        writeNums(12);
+        System.out.println();
+        //Problem 6
+        writeSquares(8);
+    }
+    public static void writeSquares(int num) {
+        if(num < 2){
+            System.out.print(num * num + ", ");
+        }else{
+            if(num % 2 == 0){
+                writeSquares( num - 1);
+                System.out.print((num - 1) * (num - 1) + ", ");
+
+            }else {
+                writeSquares( num - 1);
+                System.out.print(num * num + ", ");
+
+            }
+
+
+
+        }
+    }
+
+        public static void writeNums(int num) {
+        if(num < 2){
+            System.out.print(num + ", ");
+        }else{
+            writeNums(num - 1);
+            System.out.print(num + ", ");
+        }
 
     }
-    public static void doubleReverse(String s) {
+
+        public static void doubleReverse(String s) {
         if (s.length() > 0) {
             char c = s.charAt(s.length() - 1);
             System.out.print(c);
