8ec25b1a6e9ca8f080cbeeeff10e24975953b4c7
saussam18 <sammysau@hotmail.com>
2017-11-26 14:07:54 -0800

    Sorting hw done

---
+++ src/com/company/sorting.java
@@ -0,0 +1,139 @@
+package com.company;
+
+public class sorting{
+
+    /*
+    Pg 860
+    Problem 1: You can utilize a sequential search using a for loop or the built in function Arrays.sort then Arrays.binary search
+    Problem 2: It will only look at it less than 1% because the array continually gets cut in half
+    Problem 3: A Sequential search due to the point having nor order without a compare to method
+    Problem 4: When the array/set of data has been sorted.
+    Problem 5: A set of strings will be ordered alphabetical. To change you need to implement a compare to method
+    Problem 6: Collections.sort method wont work on a list of point objects since there is no compare to method. Implementing a compare to should fix this problem
+    Problem 7: Errors
+        1. Compare to implemented in wrong class
+        2. CompareTo actual method should take 2 accounts as parameters
+        3. Code shouldn't use this
+        4. Cannot return the subtraction of 2 doubles in compareTo
+    Problem 8: The built in method reverse Order takes the set of data and return the opposite order
+    Problem 9: O(LogN)
+    Problem 10: O(N)
+    Problem 11: O(N^2)
+    Problem 12: O(N^2)
+    Problem 13: O(N)
+    Problem 14:
+        1. O(N)
+        2. O(N^2)
+        3. O(N)
+        4. O(N)
+        5. O(LogN)
+        6. O(N^3)
+        7. O(N)
+        8. O(N)
+    Problem 15:
+        1. O(N LogN)
+        2.O(N^2)
+        3.O(N^2)(LogN)
+        4. O(N)
+        5. O(1)
+        6. O(N)
+        7. O(N!)
+    Pg 865
+    Problem 1:
+        1. 9, 9
+        2. 5, -25
+        3. 1, 1
+        4. 0, -1
+    Problem 2:
+        1. 3, -8
+        2. 7, 7
+        3. 10, 10
+        4. 0, -1
+    Problem 3:
+        1. 8, 8
+        2. 2, -5
+        3. 12, 12
+        4. 13, -95
+    Problem 4: O(N) due to only a single loop being needed to complete
+    Problem 5: O(N) due to only a single loop being needed to complete
+    Problem 6: O(N) due to only a single loop being needed to complete
+    Problem 7: O(N) due to only a single loop being needed to complete
+    Problem 8:
+        1. 63: after 54
+            9: same
+            45: after 36
+            72: top
+            27: after 18
+            18: same
+            54: same
+            36: same
+
+        2.  37: after 29
+            29: after 23
+            19: after 12
+            48: after 37
+            23: Same
+            55: Same
+            74: Same
+            12: Same
+
+    Problem 9:
+        1. Left:{63, 9, 45, 72} Right:{27, 18, 54, 36}
+        2. Left:{63, 9},{45, 72}Right:{27, 18},{54, 36}
+        3. Left:{9, 63},{45, 72}Right:{18, 27},{36, 54}
+        4. Left:{9, 45, 63, 72}Right:{18, 27, 36, 54}
+        5. {9, 18, 27, 36, 45, 54, 63, 72}
+    Problem 10:
+        1.  8: after 5
+            5: after 3
+           -9: same
+           14: after 8
+            0: after -1
+           -1: same
+           -7: same
+            3: same
+        2.  15: after 5
+            56: after 39
+            24: after 15
+            5:  after -4
+            39: after 27
+            -4: same
+            27: after 24
+            10: after 5
+
+    Problem 11:
+        1. Left:{8, 5, -9, 14} Right:{0, -1, -7, 3}
+        2. Left:{8, 5},{-9, 14}Right:{0, -1},-7, 3}
+        3. Left:{5, 8},{-9, 14}Right:{-1, 0},{-7, 3}
+        4. Left:{-9, 5, 8, 14}Right:{-7, -1, 0, 3}
+        5. {-9, -7, -1, 0, 3, 5, 8, 14}
+    Problem 12:
+     1.     22: after 11
+            44: after 33
+            11: same
+            88: after 77
+            66: after 55
+            33: after 22
+            55: after 44
+            77: after 66
+        2.  -3: after -4
+            -6: after -7
+            -1: after -2
+            -5: after -6
+             0: after -1
+            -2: after -3
+            -4: after -5
+            -7: same
+    Problem 13:
+        1. Left:{22, 44, 11, 88} Right:{66, 33, 55, 77}
+        2. Left:{22, 44},{11, 88}Right:{66, 33},{55, 77}
+        3. Left:{22, 44},{11, 88}Right:{33, 66},{55, 77}
+        4. Left:{11, 22, 44, 88}Right:{33, 55, 66, 77}
+        5. {11, 22, 33, 44, 55, 66, 77, 88}
+     */
+
+
+
+
+
+}
